a599b262017d7c6f5988c045f4f7d504aea8be72
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-04 15:00:57 +0100

    create template object js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let team = [
+    {
+        "imgteam" : "img/angela-caroll-chief-editor.jpg",
+        "nameTeam" : "Angela Carol",
+        "roleTeam" : "Chief Editor"
+    },
+
+    {
+        "imgteam" : "img/angela-lopez-social-media-manager.jpg",
+        "nameTeam" : "Angela Lopez",
+        "roleTeam" : "Social Media Manager"
+    },
+
+    {
+        "imgteam" : "img/barbara-ramos-graphic-designer.jpg",
+        "nameTeam" : "Barbara Ramos",
+        "roleTeam" : "Graphic Designer"
+    },
+
+    {
+        "imgteam" : "img/scott-estrada-developer.jpg",
+        "nameTeam" : "Scott Estrada",
+        "roleTeam" : "Developer"
+    },
+
+    {
+        "imgteam" : "img/walter-gordon-office-manager.jpg",
+        "nameTeam" : "Walter Gordon",
+        "roleTeam" : "Office Manager"
+    },
+
+    {
+        "imgteam" : "img/wayne-barnett-founder-ceo.jpg",
+        "nameTeam" : "Wayne Barnett",
+        "roleTeam" : "Founder-Ceo"
+    }
+
+]
